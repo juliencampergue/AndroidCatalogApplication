59e49843ec8a4eac84f6cb723bfd93aa5e5d47ca
Julien Campergue <julien.campergue@gmail.com>
2023-04-20 17:50:44 +0200

    Add an animation when selecting/unselecting an item in the catalog list while in expanded screen
    When in expanded screen, by default, the Catalog List takes up a certain portion of the screen
    and the content will take the rest. With this animation, the Catalog List will take up the whole
    screen if the current navigation destination is the "root" destination (ie: we are not navigating
    to any existing navigation). Then, when navigating to any destination, the Catalog List width
    will animate to the given portion of the screen and the content will animate as well to take
    up all the remaining space.

--- app/src/main/java/com/eugrepmac/catalog/MainActivity.kt
+++ app/src/main/java/com/eugrepmac/catalog/MainActivity.kt
@@ -3,6 +3,8 @@ package com.eugrepmac.catalog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
@@ -10,12 +12,27 @@ import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSiz
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.eugrepmac.catalog.ui.nav.CatalogList
 import com.eugrepmac.catalog.ui.nav.CatalogNavHost
+import com.eugrepmac.catalog.ui.nav.ROOT_ROUTE
 import com.eugrepmac.catalog.ui.theme.CatalogTheme
+
+// TODO : Put those in the theme
+/**
+ * The minimum portion of the screen that the Catalog List will take whenever we navigated to a
+ * destination (other than root) in an expanded screen.
+ */
+const val CATALOG_LIST_MIN_WIDTH_FRACTION = 0.3f
+
+/**
+ * The duration of the animation that will reduce (or augment) the catalog list width.
+ */
+const val CATALOG_LIST_WIDTH_ANIMATION_DURATION_MILLIS = 1000 // 1s
 
 /**
  * The single Activity that will handle all user interactions.
@@ -60,13 +77,27 @@ fun CatalogApp(
 
     Row(modifier = modifier) {
         if (isExpandedScreen) {
+            // Get the currently selected navigation destination for the animation described below.
+            val navBackStackEntry by navigationController.currentBackStackEntryAsState()
+            val currentDestination = navBackStackEntry?.destination
+
+            // Describe the selection animation. This animation will run when a specific route
+            // is selected from the Catalog List in expanded screen mode.
+            // By default, If no route is selected, the list will display on the whole screen width,
+            // but if we navigate to any route, then the list width will reduce to make room to the
+            // NavHost displaying the actual content. This animation allows it to be done smoothly.
+            val catalogListWidthAnimatedFraction: Float by animateFloatAsState(
+                targetValue = if (currentDestination == null || currentDestination.route == ROOT_ROUTE) 1f else CATALOG_LIST_MIN_WIDTH_FRACTION,
+                animationSpec = tween(CATALOG_LIST_WIDTH_ANIMATION_DURATION_MILLIS)
+            )
+
             // If we are on an expanded screen, we want to display two things. First thing will
             // be the list of catalog items, the other one will be the associated content.
             CatalogList(
                 navigationController = navigationController,
                 modifier = Modifier
                     .fillMaxHeight()
-                    .fillMaxWidth(0.3f)
+                    .fillMaxWidth(catalogListWidthAnimatedFraction)
             )
         }
         // The content associated to the current route will be displayed into this NavHost.
